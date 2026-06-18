7f69204011eea28a70864b8a606d0cb2856c3c6e
lahari2506 <b25ec1047@iitj.ac.in>
2026-06-18 19:56:27 +0530

    add U and U' moves

--- cube.cpp
+++ cube.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
 #include "cube.h"
 using namespace std;
-//face[0] indicates the top face containing 9 stickers we passed into
-//cw void f containing array of 9 elements
+/*MAPPING IN A CUBE:
+0 = Up
+1 = Down
+2 = Front
+3 = Back
+4 = Left
+5 = Right*/
 void rotatefaceclockwise(char f[9]){
     char temp[9];
     temp[0] = f[6];
@@ -40,14 +45,69 @@ void Cube::display(){
         cout<<endl;
     }
 }
-void Cube::U(){
+/*face[0] indicates the top face containing 9 stickers we passed into
+cw void f containing array of 9 elements*/
+void Cube::U() {
+    // rotate UP face itself CW
     rotatefaceclockwise(face[0]);
+
+    char temp[3];
+
+    // store Front top row
+    temp[0] = face[2][0];
+    temp[1] = face[2][1];
+    temp[2] = face[2][2];
+
+    // Front <- Left
+    face[2][0] = face[4][0];
+    face[2][1] = face[4][1];
+    face[2][2] = face[4][2];
+
+    // Left <- Back
+    face[4][0] = face[3][0];
+    face[4][1] = face[3][1];
+    face[4][2] = face[3][2];
+
+    // Back <- Right
+    face[3][0] = face[5][0];
+    face[3][1] = face[5][1];
+    face[3][2] = face[5][2];
+
+    // Right <- Front (saved)
+    face[5][0] = temp[0];
+    face[5][1] = temp[1];
+    face[5][2] = temp[2];
 }
+//rotate the cube U'(ACW)
+void Cube::U_prime() {
+    rotatefaceclockwise(face[0]);
+    rotatefaceclockwise(face[0]);
+    rotatefaceclockwise(face[0]);
 
+    char temp[3];
 
+    // store Front top row
+    temp[0] = face[2][0];
+    temp[1] = face[2][1];
+    temp[2] = face[2][2];
 
+    // Front <- Right
+    face[2][0] = face[5][0];
+    face[2][1] = face[5][1];
+    face[2][2] = face[5][2];
 
+    // Right <- Back
+    face[5][0] = face[3][0];
+    face[5][1] = face[3][1];
+    face[5][2] = face[3][2];
 
+    // Back <- Left
+    face[3][0] = face[4][0];
+    face[3][1] = face[4][1];
+    face[3][2] = face[4][2];
 
-
-
+    // Left <- Front
+    face[4][0] = temp[0];
+    face[4][1] = temp[1];
+    face[4][2] = temp[2];
+}
