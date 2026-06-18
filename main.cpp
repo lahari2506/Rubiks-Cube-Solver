6716a590e89094d5f12f914d3387b496283cd905
lahari2506 <b25ec1047@iitj.ac.in>
2026-06-18 19:59:29 +0530

    after U move change in main

--- main.cpp
+++ main.cpp
@@ -3,6 +3,10 @@
 using namespace std;
 int main(){
     Cube c;
+    cout<<"Initial Cube:\n";
+    c.display();
+    c.U(); //after top rotation
+    cout<<"\n After U move\n";
     c.display();
     return 0;
 }
