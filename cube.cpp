563f95f0f56491e38f4c86486c3ea2beeac039e6
lahari2506 <b25ec1047@iitj.ac.in>
2026-06-18 19:02:53 +0530

    after the top or U move

--- cube.cpp
+++ cube.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
 #include "cube.h"
 using namespace std;
+//face[0] indicates the top face containing 9 stickers we passed into
+//cw void f containing array of 9 elements
+void rotatefaceclockwise(char f[9]){
+    char temp[9];
+    temp[0] = f[6];
+    temp[1] = f[3];
+    temp[2] = f[0];
+    temp[3] = f[7];
+    temp[4] = f[4];
+    temp[5] = f[1];
+    temp[6] = f[8];
+    temp[7] = f[5];
+    temp[8] = f[2];
+
+    for(int i=0;i<9;i++){
+        f[i]=temp[i];
+    }
+
+
+}
+   
 
 Cube::Cube(){
     char colors[6]={'W','Y','G','B','O','R'};
@@ -19,6 +40,10 @@ void Cube::display(){
         cout<<endl;
     }
 }
+void Cube::U(){
+    rotatefaceclockwise(face[0]);
+}
+
 
 
 
