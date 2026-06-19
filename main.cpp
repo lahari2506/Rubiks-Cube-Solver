d1e80efe7c046de44c6b541831fc4b5f3eff762d
lahari2506 <b25ec1047@iitj.ac.in>
2026-06-19 19:48:56 +0530

    after adding R,R' followed by U,U'

--- main.cpp
+++ main.cpp
@@ -5,8 +5,26 @@ int main(){
     Cube c;
     cout<<"Initial Cube:\n";
     c.display();
-    c.U(); //after top rotation
+//test U move:
+     c.U(); //after top rotation
     cout<<"\n After U move\n";
     c.display();
+    c.U_prime();
+    cout << "\nAfter U' (should match initial):\n";
+    c.display();
+
+//RESET CUBE:
+   Cube c2;
+//test R move:
+    cout<<"Initial cube for R test:\n";
+    c2.display();
+    c2.R();
+    cout<<"after R move\n";
+    c2.display();
+    c2.R_prime();
+    cout << "\nAfter R' (should match initial):\n";
+    c2.display();
+    
+
     return 0;
 }
