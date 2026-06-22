0859866342ea72d0c076cc1ee3336ad59f677a24
lahari2506 <b25ec1047@iitj.ac.in>
2026-06-22 22:10:43 +0530

    add F,F'

--- cube.h
+++ cube.h
@@ -5,5 +5,13 @@ class Cube{
   char face[6][9];
   Cube();
   void display();
+  void U();
+  void R();
+  void L();
+  void F();
+  void U_prime();
+  void R_prime();
+  void L_prime();
+  void F_prime();
 };
 #endif
